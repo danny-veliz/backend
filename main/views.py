1820b83b4f82271396877069da738bba6f04d021
medardomoranb <119766136+medardomoranb@users.noreply.github.com>
2025-01-06 11:01:14 -0500

    s

--- main/views.py
+++ main/views.py
@@ -6,4 +6,5 @@ from django.http import HttpResponse
 
 def index(request):
     #return HttpResponse("Hello, World!")
-    return render(request, 'main/base.html')
+    #return render(request, 'main/base.html')
+    return render(request, 'main/index.html')
