104e40330ab7f3ea0f43071f739810aa2bb36d0e
Jorge2033 <jegaibor@espol.edu.ec>
2025-01-19 00:46:11 -0500

    GUIA 23

--- api/views.py
+++ api/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+ # Importe requests y json
+import requests
+import json
 
 # Create your views here.
 
@@ -42,3 +45,32 @@ class LandingAPI(APIView):
 	        
          # Devuelve el id del objeto guardado
          return Response({"id": new_resource.key}, status=status.HTTP_201_CREATED)
+
+    def index(request):
+        #Objeto con los datos a renderizar
+        data = {
+        'title': 'Landing - Dashboard',
+        }
+       # Arme el endpoint del REST API
+        current_url = request.build_absolute_uri()
+        url = current_url + '/api/v1/landing'
+
+        # Petición al REST API
+        response_http = requests.get(url)
+        response_dict = json.loads(response_http.content)
+
+        print("Endpoint ", url)
+        print("Response ", response_dict)
+
+          # Valores de la respuesta
+        responses = response_dict.values()
+
+        # Objeto con los datos a renderizar
+        data = {
+         'title': 'Landing - Dashboard',
+         'total_responses': total_responses,
+         'responses': responses
+        }
+
+        # Renderización en la plantilla
+        return render(request, 'main/index.html', data)
